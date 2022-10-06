16a74d156f4c61120067dfc685305e7cc86b8b25
halversonl <halversonleduc.college-montreal.qc.ca>
2022-10-06 16:12:02 -0400

    commit #2 6.10.22 @ 16h11

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import random
 
 niveau_vie = 20
-
+numero_monstre = 0
+numero_combat = 0
+nombre_victoires = 0
+nombre_defaites = 0
 
 def combat_monstre():
+
     force_monstre = random.randint(1, 5)
+    global numero_monstre
+    numero_monstre = numero_monstre + 1
+    global numero_combat
+    numero_combat = numero_combat + 1
+    global nombre_victoires
+    global nombre_defaites
+    global niveau_vie
 
     print("Vous tombez face à face avec un adversaire de difficulté : {}".format(force_monstre))
     reponse = input("""
@@ -15,12 +26,26 @@ def combat_monstre():
     4- Quitter la partie
     Votre réponse ici: """)
 
-    if reponse == 1:
+    if reponse == "1":
         force_joueur = random.randint(1, 6)
 
+        print("""Adversaire : {}
+        Force de l’adversaire : {}
+        Niveau de vie de l’usager : {}
+        Combat numero_combat : {} vs {}""".format(numero_monstre, force_monstre, niveau_vie, numero_combat,
+                                                  nombre_victoires, nombre_defaites))
+
+        print("Lancé du dé: {}".format(force_joueur))
+
         if force_monstre > force_joueur:
-            print("""
-            """)
+            niveau_vie = niveau_vie - 1
+            nombre_defaites = nombre_defaites +1
+            statut_partie = "Gagné"
+
+        elif force_monstre < force_joueur:
+            niveau_vie = niveau_vie + 1
+            nombre_victoires = nombre_victoires + 1
+            statut_partie = "Perdu"
 
 
 combat_monstre()
