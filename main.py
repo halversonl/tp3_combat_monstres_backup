9d4ed457bdfb861c48b5549ab22e0f2eabe1ef18
halversonl <halversonleduc.college-montreal.qc.ca>
2022-10-13 09:20:06 -0400

    commit #3 13.10.22 @ 9H18 Fonctionnel si on fait jsute #1

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import random
+import time
 
-niveau_vie = 20
+niveau_vie = 10
 numero_monstre = 0
 numero_combat = 0
 nombre_victoires = 0
 nombre_defaites = 0
+victoires_consec = 0
+statut_partie = 0
+
 
 def combat_monstre():
 
@@ -16,6 +20,8 @@ def combat_monstre():
     global nombre_victoires
     global nombre_defaites
     global niveau_vie
+    global victoires_consec
+    global statut_partie
 
     print("Vous tombez face à face avec un adversaire de difficulté : {}".format(force_monstre))
     reponse = input("""
@@ -29,23 +35,38 @@ def combat_monstre():
     if reponse == "1":
         force_joueur = random.randint(1, 6)
 
-        print("""Adversaire : {}
+        print("""
+        Adversaire #{}
         Force de l’adversaire : {}
-        Niveau de vie de l’usager : {}
-        Combat numero_combat : {} vs {}""".format(numero_monstre, force_monstre, niveau_vie, numero_combat,
-                                                  nombre_victoires, nombre_defaites))
+        Niveau de vie de l’usager avant le combat: {}
+        Combat #{} : {} victoires vs {} défaites
+        LANCÉ DU DÉ (FORCE DE L'UTILISATEUR): {}""".format(numero_monstre, force_monstre, niveau_vie,
+        numero_combat, nombre_victoires, nombre_defaites, force_joueur))
 
-        print("Lancé du dé: {}".format(force_joueur))
-
-        if force_monstre > force_joueur:
+        if force_monstre >= force_joueur:
             niveau_vie = niveau_vie - 1
-            nombre_defaites = nombre_defaites +1
-            statut_partie = "Gagné"
+            nombre_defaites = nombre_defaites + 1
+            victoires_consec = 0
+            statut_partie = "PERTE"
 
         elif force_monstre < force_joueur:
             niveau_vie = niveau_vie + 1
             nombre_victoires = nombre_victoires + 1
-            statut_partie = "Perdu"
+            victoires_consec = victoires_consec + 1
+            statut_partie = "VICTOIRE"
+
+        print("""
+        {}
+        Niveau de vie de l'usager après le combat: {}
+        Nombre de victoires consécutives: {}
+        """.format(statut_partie, niveau_vie, victoires_consec))
+
+        if niveau_vie < 1:
+            print("La partie est terminée, vous avez vaincu {} monstre(s).".format(nombre_victoires))
+
+        else:
+            # time.sleep(5)
+            combat_monstre()
 
 
 combat_monstre()
